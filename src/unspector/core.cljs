79f2a7ced8f94177614797dc35a45479a3eb8085
Paulus Esterhazy <pesterhazy@gmail.com>
2017-06-21 11:33:00 +0200

    WIP

--- src/unspector/core.cljs
+++ src/unspector/core.cljs
@@ -1,20 +1,27 @@
 (ns unspector.core
   (:require [reagent.core :as reagent :refer [atom]]
+            [clojure.string :as str]
             [goog.Uri :as guri]
             [goog.object :as gobj]))
 
 (enable-console-print!)
 
-(defn extract [qd] (into {} (map (fn [k] [k (.get qd k)]) (.getKeys qd))))
+(defn extract [qd] (map (fn [k] [k (.get qd k)]) (.getKeys qd)))
+(defn transform [kvs]
+  (reduce (fn [m [k v]]
+            (assoc-in m (str/split k #"\.") v))
+          {}
+          kvs))
 (defn get-query-params []
-  (-> (guri/parse js/location) .getQueryData extract))
+  (-> (guri/parse js/location) .getQueryData extract transform))
 
 (defn inspector [& args]
   (into [:> (gobj/get (gobj/get js/window "ReactInspector") "Inspector")] args))
 
 (defn hello-world []
   [inspector {:expand-level 3
-              :data (clj->js (get-query-params))}])
+              :data (->> (get-query-params)
+                         clj->js)}])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
