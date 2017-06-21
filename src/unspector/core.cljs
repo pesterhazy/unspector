73ffc27535a1715b71861f3e3c0fa9e5f9b70157
Paulus Esterhazy <pesterhazy@gmail.com>
2017-06-21 09:47:46 +0200

    WIP

--- src/unspector/core.cljs
+++ src/unspector/core.cljs
@@ -1,27 +1,23 @@
 (ns unspector.core
   (:require [reagent.core :as reagent :refer [atom]]
+            [goog.Uri :as guri]
             [goog.object :as gobj]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/unspector/core.cljs. Go ahead and edit it and see reloading in action.")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
+(defn extract [qd] (into {} (map (fn [k] [k (.get qd k)]) (.getKeys qd))))
+(defn get-query-params []
+  (-> (guri/parse js/location) .getQueryData extract))
 
 (defn inspector [& args]
   (into [:> (gobj/get (gobj/get js/window "ReactInspector") "Inspector")] args))
 
 (defn hello-world []
   [inspector {:expand-level 3
-              :data #js {:foo "bar"}}])
+              :data (clj->js (get-query-params))}])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
